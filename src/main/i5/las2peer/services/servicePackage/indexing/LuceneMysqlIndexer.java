6362f8fa14a8adcac626f552e6c9735716a89139
Sathvik <sathvikmail@gmail.com>
2015-03-24 15:51:40 +0100

    Added lucene Indexer and searcher class

---
+++ src/main/i5/las2peer/services/servicePackage/indexing/LuceneMysqlIndexer.java
@@ -0,0 +1,84 @@
+/**
+ * 
+ */
+package i5.las2peer.services.servicePackage.indexing;
+
+import i5.las2peer.services.servicePackage.database.Data;
+import i5.las2peer.services.servicePackage.utils.Global;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.DaoManager;
+import com.j256.ormlite.support.ConnectionSource;
+
+/**
+ * @author sathvik
+ *
+ */
+public class LuceneMysqlIndexer {
+	private ConnectionSource connSrc;
+	private IndexWriter indexWriter;
+
+	public LuceneMysqlIndexer(ConnectionSource connectionSrc)
+			throws IOException {
+		connSrc = connectionSrc;
+		Directory indexDir = FSDirectory.open(new File("indexDirectory")
+				.toPath());
+		IndexWriterConfig config = new IndexWriterConfig(new StandardAnalyzer());
+		indexWriter = new IndexWriter(indexDir, config);
+	}
+
+	public void buildIndex() throws SQLException, IOException {
+		Document doc;
+
+		Dao<Data, Long> postsDao = DaoManager.createDao(connSrc, Data.class);
+		List<Data> data_entites = postsDao.queryForAll();
+		Global.totalNoOfResources = data_entites.size();
+		StringBuffer fullSearchableText;
+
+		for (Data entity : data_entites) {
+			doc = new Document();
+			double postId = entity.getPostId();
+			String title = entity.getTitle();
+			String body = entity.getBody();
+			double parentId = entity.getParentId();
+
+			fullSearchableText = new StringBuffer();
+			if (title != null && title.length() > 0) {
+				fullSearchableText.append(title + " ");
+			}
+
+			if (body != null && body.length() > 0) {
+				fullSearchableText.append(body);
+			}
+
+			doc.add(new StringField("postid", String.valueOf(postId),
+					Field.Store.YES));
+			doc.add(new StringField("parentid", String.valueOf(parentId),
+					Field.Store.YES));
+			doc.add(new TextField("searchableText", fullSearchableText
+					.toString(), Field.Store.YES));
+
+			indexWriter.addDocument(doc);
+		}
+
+		if (indexWriter != null) {
+			indexWriter.close();
+		}
+
+	}
+}
---
+++ src/main/i5/las2peer/services/servicePackage/indexing/LuceneSearcher.java
@@ -0,0 +1,70 @@
+/**
+ * 
+ */
+package i5.las2peer.services.servicePackage.indexing;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.store.FSDirectory;
+
+/**
+ * @author sathvik
+ *
+ */
+public class LuceneSearcher {
+	private IndexSearcher searcher = null;
+	private QueryParser parser = null;
+	private String queryString = null;
+
+	/** Creates a new instance of SearchEngine */
+	public LuceneSearcher(String query) throws IOException {
+		searcher = new IndexSearcher(DirectoryReader.open(FSDirectory
+				.open(new File("indexDirectory").toPath())));
+		parser = new QueryParser("searchableText", new StandardAnalyzer());
+		queryString = query;
+	}
+
+	public TopDocs performSearch(String queryString, int n) throws IOException,
+			ParseException {
+		Query query = parser.parse(queryString);
+		return searcher.search(query, n);
+	}
+
+	public ArrayList<String> getQueryTermsAsStrings() throws ParseException {
+		Set<Term> terms = new HashSet<Term>();
+		if (queryString != null) {
+			Query query = parser.parse(queryString);
+			query.extractTerms(terms);
+		}
+
+		ArrayList<String> qTerms = new ArrayList<String>();
+		if (terms != null && terms.size() > 0) {
+			java.util.Iterator<Term> it = terms.iterator();
+			while (it.hasNext()) {
+				qTerms.add(it.next().toString());
+			}
+
+			return qTerms;
+		}
+
+		return null;
+	}
+
+	public Document getDocument(int docId) throws IOException {
+		return searcher.doc(docId);
+	}
+}
+
