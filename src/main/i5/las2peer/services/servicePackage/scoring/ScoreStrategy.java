4561ceb18fd8929f97fa013d4690cb0f759d0dac
Sathvik <sathvikmail@gmail.com>
2015-03-30 01:19:52 +0200

    Updated Javadocs

--- src/main/i5/las2peer/services/servicePackage/scoring/ScoreStrategy.java
+++ src/main/i5/las2peer/services/servicePackage/scoring/ScoreStrategy.java
@@ -3,14 +3,32 @@ package i5.las2peer.services.servicePackage.scoring;
 import java.util.LinkedHashMap;
 
 /**
+ * A strategy pattern to execute a particular scoring algorithm
+ * 
+ * @see <a href="http://en.wikipedia.org/wiki/Strategy_pattern">Strategy
+ *      Pattern</a>
+ * 
  * @author sathvik
  */
 
 public interface ScoreStrategy {
-	void executeAlgorithm();
 
-	String getExperts();
+    /**
+     * A method where the logic of algorithm is implemented
+     */
+    void executeAlgorithm();
 
-	LinkedHashMap<String, Double> getExpertMap();
+    /**
+     * 
+     * @return String in the form of json containing details about the experts
+     */
+    public String getExperts();
+
+    /**
+     * 
+     * @return A map of expert id to their score calculated by the respective
+     *         algorithm.
+     */
+    public LinkedHashMap<String, Double> getExpertMap();
 
 }
--- src/main/i5/las2peer/services/servicePackage/scoring/ScoringContext.java
+++ src/main/i5/las2peer/services/servicePackage/scoring/ScoringContext.java
@@ -3,25 +3,52 @@ package i5.las2peer.services.servicePackage.scoring;
 import java.util.LinkedHashMap;
 
 /**
+ * A context for the startegy pattern. This sets the type of strategy to be used
+ * to execute algorithm.
+ * 
+ * 
+ * @see <a href="http://en.wikipedia.org/wiki/Strategy_pattern">Strategy
+ *      Pattern</a>
+ * 
+ * 
  * @author sathvik
  */
 
 public class ScoringContext {
-	private ScoreStrategy strategy;
+    private ScoreStrategy strategy;
 
-	public ScoringContext(ScoreStrategy strategy) {
-		this.strategy = strategy;
-	}
+    /**
+     * This method sets up the strategy to be used.
+     * 
+     * @param strategy
+     * @see ScoreStrategy
+     */
+    public ScoringContext(ScoreStrategy strategy) {
+	this.strategy = strategy;
+    }
 
-	public void executeStrategy() {
-		this.strategy.executeAlgorithm();
-	}
+    /**
+     * Method to be called while executing the respective algorithm.
+     * 
+     */
+    public void executeStrategy() {
+	this.strategy.executeAlgorithm();
+    }
 
-	public String getExperts() {
-		return this.strategy.getExperts();
-	}
+    /**
+     * 
+     * @return A Json string containing details about the experts.
+     */
+    public String getExperts() {
+	return strategy.getExperts();
+    }
 
-	public LinkedHashMap<String, Double> getExpertMap() {
-		return this.strategy.getExpertMap();
-	}
+    /**
+     * 
+     * @return A mapping from expert id to their score value as calculated by
+     *         the algorithm.
+     */
+    public LinkedHashMap<String, Double> getExpertMap() {
+	return strategy.getExpertMap();
+    }
 }
