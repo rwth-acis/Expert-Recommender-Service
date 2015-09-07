4ee153232b4d664e4e39484b686a79ddc020484c
Sathvik <sathvikmail@gmail.com>
2015-09-07 02:27:16 +0200

    Updated sql script

--- scripts.sql
+++ scripts.sql
@@ -1,1 +1,25 @@
-Insert into nature.data (post_id, parent_id, creation_date, body, owner_user_id, title)  select post_id, topic_id, post_time, post_text, poster_id, post_subject from est_nature.phpbb_posts
+--
+-- Table structure for table `datasetInfo`
+--
+
+CREATE TABLE IF NOT EXISTS `datasetInfo` (
+`Id` int(11) NOT NULL,
+  `dataset_name` text NOT NULL,
+  `index_path` text NOT NULL,
+  `created_on` text NOT NULL,
+  `database_name` text NOT NULL,
+  `original_filepath` text NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for table `datasetInfo`
+--
+ALTER TABLE `datasetInfo`
+ ADD PRIMARY KEY (`Id`);
+
+
+--
+-- AUTO_INCREMENT for table `datasetInfo`
+--
+ALTER TABLE `datasetInfo`
+MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
